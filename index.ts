ac45ed6fc72ceb1c8d00e57a11877410b8df5a29
Habib Ur Rehman <madnihabib6@gmail.com>
2024-04-07 23:47:50 +0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+let greeting = "Hello, World!";
+console.log(greeting)
+
+let num1: number, num2: number;
+num1 = 2;
+num2 = 3;
+let sum = num1 + num2;
+console.log(sum)
+let differ = num2 - num1;
+console.log(differ)
+let prod = num1 * num2;
+console.log(prod)
+let quote = num1 ** num2;
+console.log(quote)
+
+let a = 2;
+let b = 5;
+a = a + b;
+b = a - b;
+a = a - b;
+console.log(a);
+console.log(b);
